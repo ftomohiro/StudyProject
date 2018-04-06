b4722e832e98b2db4b1771bd507ced7b7e295da7
harumi_PCMac <harumi_pcmac@harumi-PCMacnoMacBook-Pro.local>
2018-04-06 14:27:20 +0900

    課題3コミット

--- study_project/src/main/java/masukura/opst/co/jp/studyproject/MainActivity.java
+++ study_project/src/main/java/masukura/opst/co/jp/studyproject/MainActivity.java
@@ -6,8 +6,19 @@ import android.widget.Button;
 import android.widget.Toast;
 import android.view.View;
 import android.view.Gravity;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent event) {
+		if (event.getAction() == KeyEvent.ACTION_DOWN) {
+			switch (event.getKeyCode()) {
+				case KeyEvent.KEYCODE_BACK:
+					return true;
+			}
+		}
+		return super.dispatchKeyEvent(event);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +30,7 @@ public class MainActivity extends AppCompatActivity {
 		button1.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				if (view == button1) {
-					Toast toast1 = Toast.makeText(MainActivity.this, "ボタン1を押下しました", Toast.LENGTH_SHORT);
-					toast1.setGravity(Gravity.CENTER, 0, -200);
-					toast1.show();
+					finishAndRemoveTask();
 				}
 			}
 		});
